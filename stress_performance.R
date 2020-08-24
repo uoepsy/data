696bc63736a3ae335b5ba2281b5cc071eef7c3c1
Umberto Noe <9914422+unoe@users.noreply.github.com>
2020-08-24 09:33:47 +0100

    Add gitignore

--- stress_performance.R
+++ stress_performance.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 
-n = 30
+set.seed(1)
 
+n <- 30
 df <- tibble(
     stress = runif(n, 3, 10),
     performance = -(1.2 * stress - 15)^2 + rnorm(n, 0, 3)
 )
+
 ggplot(df, aes(stress, performance)) +
     geom_point()
 
