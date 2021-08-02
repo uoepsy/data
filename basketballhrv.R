17a90f30131cced4f2113a479055101ea233eb87
Josiah King <josiahpjking@gmail.com>
2021-08-02 11:33:17 +0100

    update bball dapr3 lab

--- basketballhrv.R
+++ basketballhrv.R
@@ -20,7 +20,11 @@ lp = (4 + re) + (.3 + re_x)*x + .45*b -.2*b*x + 0.6*jx
 y = rnorm(N, mean = lp, sd = sigma)               # create a continuous target variable
 y_bin = rbinom(N, size = 1, prob = plogis(lp))    #- create a binary target variable
 
-hrvdat = tibble(stakes=21-as.numeric(cut(x,20)), condition = as.character(factor(b, labels=c("money","kudos"))), hrv = y, hr_abv = y_bin, sub = factor(g), t = jx)
+hrvdat = tibble(stakes=21-as.numeric(cut(x,20)), condition = as.character(factor(b, labels=c("money","kudos"))), hrv = y, hr_abv = y_bin, sub = factor(g), throw = jx) %>%
+    group_by(sub) %>%
+    mutate(trial_no = sample(1:20), 
+           success_rate = cumsum(throw)/trial_no) %>% ungroup %>% 
+    arrange(sub,trial_no)
 
 ggplot(hrvdat, aes(x=stakes,y=hrv,group=sub,col=factor(condition)))+geom_smooth(method="lm",se=F, alpha=.2)+
   geom_point()+NULL
