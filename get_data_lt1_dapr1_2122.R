8fd0292ffaebd8fc7bfcc388eabc33f30993f341
Umberto Noe <9914422+unoe@users.noreply.github.com>
2021-11-23 15:26:49 +0000

    lt1 data

---
+++ get_data_lt1_dapr1_2122.R
@@ -0,0 +1,43 @@
+require(tidyverse)
+
+if ( exists("params") ) {
+    tryCatch({
+        examNumber = as.numeric(gsub("[^\\d]+", "", params$examnumber, perl=TRUE))
+    }, error = function(e) {print("NOT A VALID EXAM NUMBER. CHANGE IT IN LINE 6")}
+    )
+} else {
+    examNumber = 8675309
+}
+
+get_labtest_data = function(examNumber) {
+    
+    set.seed(examNumber)
+    
+    df = read_csv("https://uoepsy.github.io/data/lock5watertaste.csv")
+    df$AgeGroup = cut(df$Age, breaks = c(0, 18, 50), labels = c("Non-adult", "Adult"))
+    df = df %>% select(AgeGroup, UsuallyDrink)
+    
+    tbl = table(df$AgeGroup, df$UsuallyDrink)
+    ptbl = addmargins(tbl)
+    
+    R = ptbl[1:2, 4]
+    C = ptbl[3, 1:3]
+    TBL = r2dtable(1, R, C)[[1]]
+    
+    dimnames(TBL) = dimnames(tbl)
+    TBL
+    tbl
+    
+    out = as.data.frame(as.table(TBL)) %>%
+        uncount(Freq) %>%
+        rename(AgeGroup = Var1, UsuallyDrink = Var2) %>%
+        sample_n(size = nrow(.), replace = FALSE)
+    
+    out = tibble(out)
+
+    return(out)
+}
+
+water <- get_labtest_data(examNumber)
+
+rm(examNumber, get_labtest_data)
